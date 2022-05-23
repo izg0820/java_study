485600bd202675538f8831057b0f2b0d8b7110ec
izg0820 <izg0820@naver.com>
2022-05-23 22:31:58 +0900

    exception의 클린코드 내용 추가

---
+++ src/main/java/com/example/exception/CleanExceptionSample.java
@@ -0,0 +1,56 @@
+package com.example.exception;
+
+import model.Member;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
+public class CleanExceptionSample {
+
+    //오류코드보다는 예외를 사용하라
+    public void useErrorCode() {
+        Member member = new Member("김나박이", 11);
+        if (member.isValidateErrorCode() != Member.ERROR.LENGTH_OVER) {
+            if (member.doSomething() != Member.ERROR.FAILURE) {
+                //...
+            } else {
+                System.out.printf("Wrong action");
+            }
+        } else {
+            System.out.printf("Invalid Member Name");
+        }
+    }
+
+    public void useException() {
+        try{
+            createMember("김나박이", 11);
+        } catch (MemberException e) {
+            //...
+        }
+    }
+
+    public Member createMember(String name, int age) {
+        Member member = new Member(name, age);
+        if(member.isValidateException()) {
+            throw new MemberException("Invalid Member Name");
+        }
+        return member;
+    }
+
+    //Unchecked 예외를 사용해라
+    public void checked() {
+        try {
+            A();
+        } catch (SQLException | FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void A() throws SQLException, FileNotFoundException {
+        B();
+    }
+
+    public void B() throws SQLException {
+        //do something
+    }
+}
